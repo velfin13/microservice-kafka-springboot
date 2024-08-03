7c4902ea8ed7b5aca69bcd9979db7ee53a8dfd2c
Velfin Velasquez <velfinvelasquez@gmail.com>
2024-08-02 22:29:09 -0500

    implementado el gateway y el eureka server

---
+++ eureka-server/src/test/java/me/velfinvelasquez/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.velfinvelasquez.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
